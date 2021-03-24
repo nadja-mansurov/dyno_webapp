94c9f2260a85d360763517a876aeaec8a2ded0d5
Nadja Mansurov <nadja.mansurov@gmail.com>
2021-03-24 19:30:05 +0100

    initial flask & vue apps

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # dyno_webapp
+
+
+    cd dynophores_webapp
+
+## activate conda 
+    conda source activate dynophores_webapp
+
+## start sever
+    cd server
+    flask run -p 3000
+
+Check in browser `http://localhost:3000/`
+
+## start client
+    cd client
+    npm install
+    npm run serve
+
+Check in browser `http://localhost:8080/`
---
+++ client/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h4>Bootstrap test <i class="bi-alarm"></i></h4>
+  <div class="d-grid gap-2">
+    <button type="button" class="btn btn-primary btn-sm px-4 me-sm-3">Primary</button>
+    <button type="button" class="btn btn-outline-secondary btn-sm px-4">Default</button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app { 
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50; 
+  margin-top: 60px; 
+}
+</style>
