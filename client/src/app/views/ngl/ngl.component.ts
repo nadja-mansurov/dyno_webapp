f2968d63be2ef4bf6eefb30574149870be60bce8
Nadja Mansurov <nadja.mansurov@gmail.com>
2021-04-03 13:47:07 +0200

    allow user to set structure id

--- client/src/app/views/ngl/ngl.component.ts
+++ client/src/app/views/ngl/ngl.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+const URI = 'rcsb://'
 
 @Component({
   selector: 'dyno-ngl',
@@ -6,7 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ngl.component.scss']
 })
 export class NglComponent implements OnInit {
+  public fileAddressInput = new FormControl('');
+  public visualizedId = "1CRN";
+  public isFailed = false;
+
+  private fileAddress = "1CRN";
   private Stage: any;
+  private stageInstance: any;
 
   constructor() {
     // TODO: check update https://github.com/nglviewer/ngl,
@@ -15,8 +24,21 @@ export class NglComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    let stage = new this.Stage("viewport");
-    stage.loadFile("rcsb://1crn", {defaultRepresentation: true});
+    this.fileAddressInput.setValue(this.fileAddress);
+    this.stageInstance = new this.Stage("viewport");
+    this.stageInstance.loadFile(URI + this.fileAddress, {defaultRepresentation: true});
+  }
+
+  visualize() {
+    this.fileAddress = this.fileAddressInput.value;
+    this.stageInstance.loadFile(URI + this.fileAddress, {defaultRepresentation: true})
+      .then((t: any) => {
+        this.visualizedId = t.parameters.name;
+        this.isFailed = false;
+      })
+      .catch(() => {
+        this.isFailed = true;
+      });
   }
 
 }
